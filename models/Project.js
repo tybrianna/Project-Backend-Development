6b4b2fcf88dd425a8885e6779bef2338a5a239c8
tybrianna <tybriannahall@gmail.com>
2026-05-28 13:53:28 -0400

    added projectSchema to project models file to include name, descriptopn, user, and timestamps

---
+++ models/Project.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Project', projectSchema);
